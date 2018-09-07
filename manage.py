bd7639b79bc82dab1e88d4a35df07648f1e7ec86
EugeneZnm <eugenenzioki@gmail.com>
2018-09-07 16:55:55 +0300

    creating app instance, instantiating manager class, command to launch application server, initialising migrate class in app instance

--- manage.py
+++ manage.py
@@ -4,3 +4,16 @@ from app.models import User, Pitches, Comments, Upvote, Downvote, Roles
 from flask_script import User, Role
 from flask_script import Manager, Server
 
+# Creating app instance
+app = create_app('development')
+
+# instantiate manager class
+manager =Manager(app)
+
+# command to launch application server
+manager.add_command('server', Server)
+
+#initialise migrate class in app instance
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
