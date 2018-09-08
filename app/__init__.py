c901494f1e7f08277fa8b007958c5959812ecfe9
EugeneZnm <eugenenzioki@gmail.com>
2018-09-09 00:50:11 +0300

    intialising flask login

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,12 @@ from flask import Flask
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 
+# creating instance of Login
+login_manager= LoginManager()
+l0gin_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 db = SQLAlchemy()
 bootstrap = Bootstrap()
@@ -16,7 +21,12 @@ def create_app(config_name):
     """
     app = Flask(__name__)
     app.config.from_object(config_options[config_name])
+    # initialising bootstrap
     bootstrap.init_app(app)
+    # initialising db model
+    db.init_app(app)
+    # initialising flask login
+    login_manager.init_app(app)
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix = '/authenticate')
