d09d6ef5d8de8e27d7a2d4f33dd935bc4886be61
EugeneZnm <eugenenzioki@gmail.com>
2018-09-08 18:54:03 +0300

    adding password generating and checking functions

--- app/models.py
+++ app/models.py
@@ -30,6 +30,18 @@ class User(db.Model):
     creating columns, with primary key set as true
     
     """
+    pass_secure = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('YOU CANNOT READ THE PASSWORD')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(password)
 
     def __repr__(self):
         """
