1b31f5646cdba710abe3b45a4f214f9fd7184112
EugeneZnm <eugenenzioki@gmail.com>
2018-09-09 02:37:27 +0300

    update login html template

--- app/auth/views.py
+++ app/auth/views.py
@@ -30,7 +30,7 @@ def login():
         flash('Invalid username or Password')
 
     title = "watchlist login"
-    return render_template('auth/login.html', login_form, title=title)
+    return render_template('auth/login.html', login_form=login_form, title=title)
 
 
 @auth.route('register', methods=['GET', "POST"])
