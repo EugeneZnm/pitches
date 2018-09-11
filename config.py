98693595b0eea24a65fffbe404c178a87a7ee653
EugeneZnm <eugenenzioki@gmail.com>
2018-09-11 16:57:48 +0300

    adeployment

--- config.py
+++ config.py
@@ -7,12 +7,19 @@ class Config:
 
     """
 
-
-
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://eugene:necromancer@localhost/pit1'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CON = True
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    # app.config['SECRET_KEY'] = 'any secret string'
 
 
 class ProdConfig(Config):
@@ -20,14 +27,7 @@ class ProdConfig(Config):
     production configuration child class
     """
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-
+    pass
 
 
 class TestConfig(Config):
@@ -41,6 +41,7 @@ class DevConfig(Config):
     """
     development configuration child class
     """
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://eugene:necromancer@localhost/pit1'
     DEBUG = True
 
 
